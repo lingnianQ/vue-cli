93059596dc32b2edabd29c5982558f4656062b20
lingnian <sytsnb@gmail.com>
2022-10-09 01:32:05 +0800

    init

--- src/components/PhotoAdd.vue
+++ src/components/PhotoAdd.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="photo">
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="名称" prop="photoName">
-        <el-input v-model="ruleForm.photoName"></el-input>
+      <el-form-item label="名称" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
       </el-form-item>
-      <el-form-item label="简介" prop="introduction">
-        <el-input v-model="ruleForm.introduction"></el-input>
+      <el-form-item label="简介" prop="description">
+        <el-input v-model="ruleForm.description"></el-input>
       </el-form-item>
-      <el-form-item label="排序序号" prop="id">
-        <el-input v-model="ruleForm.id"></el-input>
+      <el-form-item label="排序序号" prop="sort">
+        <el-input v-model="ruleForm.sort"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">添加</el-button>
@@ -24,20 +24,19 @@ export default {
   data() {
     return {
       ruleForm: {
-        photoName: '',
-        introduction: '',
-        id: ''
+        name: '',
+        description: '',
+        sort: ''
       },
       rules: {
-        photoName: [
+        name: [
           {required: true, message: '请输入相册名', trigger: 'blur'},
           {min: 1, max: 15, message: '长度在 1 到 15 个字符', trigger: 'blur'}
         ],
-        introduction: [
+        description: [
           {required: true, message: '请输入简介', trigger: 'blur'},
           {min: 1, max: 15, message: '长度在 1 到 15 个字符', trigger: 'blur'}
         ],
-        id: []
       }
     };
   },
@@ -45,6 +44,14 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
+          let url = 'http://127.0.0.1:8081/albums/add-new';
+          let formData = this.qs.stringify(this.ruleForm);
+
+          this.axios.post(url,formData).then((res) => {
+            if (res.data.state==2000) {
+              this.$message.success("添加相册成功")
+            }
+          })
           console.log('add!!');
         } else {
           console.log('error submit!!');
